f60fbbc259c415e4b50b3d3d863fe8667a914a7e
Pavel <19418601+rakleed@users.noreply.github.com>
2023-11-30 06:58:32 +0300

    feat(front): add promo notification at home page

--- frontend/src/pages/Diary/Diary.jsx
+++ frontend/src/pages/Diary/Diary.jsx
@@ -1,25 +1,15 @@
 import { Header } from '../../components/Header/Header';
 import { SidePanel } from '../../components/SidePanel/SidePanel';
 import './Diary.sass';
-import Header from '../../components/Header/Header.jsx';
-import SidePanel from '../../components/SidePanel/SidePanel';
+import { PromoNotification } from '../../components/PromoNotification/PromoNotification';
 
-function Diary(props) {
-    return (
-        <>
-            <Header />
-            <SidePanel />
-            <main className="content">
-                
-            </main>
-        </>
-    );
 function Diary(properties) {
   return (
     <>
       <Header />
       <SidePanel />
       <main className="content">
+        <PromoNotification />
       </main>
     </>
   );
