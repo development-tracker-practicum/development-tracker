473f1ef592c459df7b1775c0709c668f38721d08
Petr Oganin <necroshizo@gmail.com>
2023-12-03 00:51:18 +0300

    new db

--- backend/course/admin.py
+++ backend/course/admin.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 
-from .models import Course, Skill, SkillsTrack, Tag, Track, UserTrack
+from .models import Course, Skill, Theme, Tag, Modul, Special, Grade
 
 
 admin.site.register(Course)
 admin.site.register(Skill)
-admin.site.register(SkillsTrack)
+admin.site.register(Theme)
 admin.site.register(Tag)
-admin.site.register(Track)
-admin.site.register(UserTrack)
+admin.site.register(Modul)
+admin.site.register(Special)
+admin.site.register(Grade)
--- backend/course/models.py
+++ backend/course/models.py
@@ -6,6 +6,40 @@ from django.utils.translation import gettext_lazy as _
 User = get_user_model()
 
 
+class BaseProduct(models.Model):
+
+    name = models.CharField(
+        verbose_name=_("Назване"),
+        unique=True,
+        blank=False,
+        max_length=200
+    )
+    description = models.TextField(
+        verbose_name=_("Описание"),
+    )
+    images = models.ImageField(
+        verbose_name=_("Превью"),
+        upload_to="img/",
+        null=True,
+        blank=False
+    ) 
+    price = models.DecimalField(
+        verbose_name=_("Стоимость"),
+        decimal_places = 2,
+        max_digits=13,
+    )
+    studying_time = models.DurationField(
+        verbose_name=_("Время обучения"),
+        blank=False,
+    )
+
+    class Meta:
+        abstract = True
+
+    def __str__(self):
+        return self.name
+
+
 class Tag(models.Model):
     name = models.CharField(
         verbose_name=_("Тэг"),
@@ -23,41 +57,35 @@ class Tag(models.Model):
         return self.name
 
 
-class Course(models.Model):
+class Grade(models.Model):
     name = models.CharField(
-        verbose_name=_("Назване курса"),
+        verbose_name=_("Грейт"),
         unique=True,
         blank=False,
         max_length=200
     )
-    description = models.TextField(
-        verbose_name=_("Описание курса"),
-    )
-    tags = models.ManyToManyField(
-        Tag,
-        related_name="curse",
-        verbose_name=_("Тэги курса"),
-    )
-    images = models.ImageField(
-        verbose_name=_("Превью курса"),
-        upload_to="img/",
-        null=True,
-        blank=False
-    ) 
-    price = models.DecimalField(
-        verbose_name=_("Стоимость курса"),
-        decimal_places = 2,
-        max_digits=13,
-    )
-    studying_time = models.DurationField(
-        verbose_name=_("Время обучения"),
-        blank=False,
-    )
 
     class Meta:
         ordering = ["name"]
-        verbose_name = _("Курс")
-        verbose_name_plural = _("Курсы")
+        verbose_name = _("Грейт")
+        verbose_name_plural = _("Грейты")
+
+    def __str__(self):
+        return self.name
+
+
+class Special(models.Model):
+    title = models.CharField(
+        verbose_name=_("Профессия"),
+        unique=True,
+        blank=False,
+        max_length=200
+    )
+
+    class Meta:
+        ordering = ["title"]
+        verbose_name = _("Профессия")
+        verbose_name_plural = _("Профессии")
 
     def __str__(self):
         return self.name
@@ -81,51 +109,142 @@ class Skill(models.Model):
         return self.name
 
 
-class Track(models.Model):
-    name = models.CharField(
-        verbose_name=_("Специализация"),
-        max_length=200,
+class UserLevel(models.Model):
+    user_id = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="user_level",
+        verbose_name=_("Пользователь"),
     )
-    course = models.ManyToManyField(
-        Course,
-        related_name="track",
-        verbose_name=_("Курсы"),
+    level_id = models.ForeignKey(
+        "Level",
+        on_delete=models.CASCADE,
+        related_name="user_level",
+        verbose_name=_("Уровень"),
+    )
+
+    class Meta:
+        ordering = ["user_id"]
+        verbose_name = _("Направление пользователя")
+        verbose_name_plural = _("Направления пользователя")
+
+    def __str__(self):
+        return f"{self.user_id} {self.level_id}"
+
+
+class Level(models.Model):
+
+    special_id = models.ForeignKey(
+        Special,
+        on_delete=models.CASCADE,
+        related_name="level",
+        verbose_name=_("Профессия"),
+    )
+    grade_id = models.ForeignKey(
+        Grade,
+        on_delete=models.CASCADE,
+        related_name="level",
+        verbose_name=_("Грейт"),
+    )
+    skill_id = models.ManyToManyField(
+        Skill,
+        related_name="level",
+        verbose_name=_("Навыки"),
+    )
+    class Meta:
+        ordering = ["grade_id"]
+        verbose_name = _("Уровень професии")
+        verbose_name_plural = _("Уровни профессии")
+
+    def __str__(self):
+        return f"{self.special_id} {self.grade_id}"
+
+
+class Course(BaseProduct):
+
+    tags = models.ManyToManyField(
+        Tag,
+        related_name="curse",
+        verbose_name=_("Тэги"),
+    )
+    level_id = models.ForeignKey(
+        Level,
+        on_delete=models.CASCADE,
+        related_name="curse",
+        verbose_name=_("Уровень"),
+    )
+    modul_id = models.ManyToManyField(
+        Tag,
+        related_name="curse",
+        verbose_name=_("Модули"),
     )
 
     class Meta:
         ordering = ["name"]
-        verbose_name = _("Направление")
-        verbose_name_plural = _("Направления")
+        verbose_name = _("Курс")
+        verbose_name_plural = _("Курсы")
+    
 
-    def __str__(self):
-        return self.name
+class Modul(BaseProduct):
+
+    tags = models.ManyToManyField(
+        Tag,
+        related_name="modul",
+        verbose_name=_("Тэги"),
+    )
+    level_id = models.ForeignKey(
+        Level,
+        on_delete=models.CASCADE,
+        related_name="modul",
+        verbose_name=_("Уровень"),
+    )
+    theme = models.ManyToManyField(
+        "Theme",
+        related_name="modul",
+        verbose_name=_("Темы"),
+    )
+
+    class Meta:
+        ordering = ["name"]
+        verbose_name = _("Модуль")
+        verbose_name_plural = _("Модули")
 
 
-class UserTrack(models.Model):
+class Theme(BaseProduct):
 
-    class Greate(models.TextChoices):
-        JUNIOR = "J",_("Junior")
-        MIDDLE = "M",_("Middle")
-        SENIOR = "S",_("Senior")
+    tags = models.ManyToManyField(
+        Tag,
+        related_name="them",
+        verbose_name=_("Тэги"),
+    )
+    level_id = models.ForeignKey(
+        Level,
+        on_delete=models.CASCADE,
+        related_name="them",
+        verbose_name=_("Уровень"),
+    )
 
-    user =  models.ForeignKey(
+    class Meta:
+        ordering = ["name"]
+        verbose_name = _("Темы")
+        verbose_name_plural = _("Темы")
+
+
+class UserSkill(models.Model):
+
+    user_id =  models.ForeignKey(
         User,
         on_delete=models.CASCADE,
-        related_name="user_track",
+        related_name="user_skill",
         verbose_name=_("Пользователь"),
     )
-    track =  models.ForeignKey(
-        Track,
+    skill_id =  models.ForeignKey(
+        Skill,
         on_delete=models.CASCADE,
-        related_name="user_track",
-        verbose_name=_("Направление"),
+        related_name="user_skill",
+        verbose_name=_("Навык"),
     )
-    greate =  models.CharField(
-        verbose_name=_("Квалификация"),
-        max_length=1,
-        choices=Greate.choices,
-        default=Greate.JUNIOR,
-    )
+
     accordance = models.PositiveIntegerField(
         verbose_name=_("Процент соответствия"),
         validators=[
@@ -140,43 +259,8 @@ class UserTrack(models.Model):
 
     class Meta:
         ordering = ["accordance"]
-        verbose_name = _("Направление пользователя")
-        verbose_name_plural = _("Направления пользователя")
+        verbose_name = _("Навык пользователя")
+        verbose_name_plural = _("Навыки пользователя")
 
     def __str__(self):
-        return f"{self.user} {self.greate} {self.accordance}%"
-
-
-class SkillsTrack(models.Model):
-
-    track = models.ForeignKey(
-        Track,
-        on_delete=models.CASCADE,
-        related_name="skills_track",
-        verbose_name=_("Специализация"),
-    )
-    skills = models.ForeignKey(
-        Skill,
-        on_delete=models.CASCADE,
-        related_name="skills_track",
-        verbose_name=_("Навык"),
-    )
-    level = models.PositiveIntegerField(
-        verbose_name=_("Процент соответствия"),
-        validators=[
-            MinValueValidator(
-                limit_value=1, message=_("Минимальный процент навыка")
-            ),
-            MaxValueValidator(
-                limit_value=100, message=_("Максимальный процент навыка")
-            )
-        ],
-    )
-
-    class Meta:
-        ordering = ["track"]
-        verbose_name = _("Навык для направления")
-        verbose_name_plural = _("Навыки для направления")
-
-    def __str__(self):
-        return f"{self.track} {self.skills} {self.level}%"
+        return f"{self.user_id} {self.skill_id} {self.accordance}%"
